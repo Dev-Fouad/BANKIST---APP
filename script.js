38d136af0759da4a558580d34ebda006ea324b18
fouad ajayi <fouadajayi6@gmail.com>
2022-11-09 23:11:21 +0100

    Implementing Functionalities

--- script.js
+++ script.js
@@ -65,12 +65,36 @@ const inputClosePin = document.querySelector('.form__input--pin');
 /////////////////////////////////////////////////
 // LECTURES
 
-const currencies = new Map([
-  ['USD', 'United States dollar'],
-  ['EUR', 'Euro'],
-  ['GBP', 'Pound sterling'],
-]);
+// const currencies = new Map([
+//   ['USD', 'United States dollar'],
+//   ['EUR', 'Euro'],
+//   ['GBP', 'Pound sterling'],
+// ]);
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
+
+let displayMovements = function (movements) {
+
+  containerMovements.innerHTML = '';
+
+  movements.forEach((mov , i) => {
+
+    let type = mov > 0 ? 'deposit' : 'withdrawal'
+
+    let html = `
+
+    <div class="movements__row">
+      <div class="movements__type movements__type--${type}">${i + 1} ${type}</div>
+      <div class="movements__value">${mov}</div>
+    </div>
+
+    `
+
+    containerMovements.insertAdjacentHTML('afterbegin' , html)
+  });
+
+}
+ 
+displayMovements(account1.movements)
