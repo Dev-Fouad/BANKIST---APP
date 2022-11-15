12e9dd1560f78deb5bcf67173858570a4bf85520
fouad ajayi <fouadajayi6@gmail.com>
2022-11-15 16:39:49 +0100

    New changes

--- script.js
+++ script.js
@@ -43,14 +43,14 @@ const account1 = {
   pin: 1111,
 
   movementsDates: [
-    "2019-11-18T21:31:17.178Z",
-    "2019-12-23T07:42:02.383Z",
-    "2020-01-28T09:15:04.904Z",
-    "2020-04-01T10:17:24.185Z",
-    "2020-05-08T14:11:59.604Z",
-    "2020-07-26T17:01:17.194Z",
-    "2020-07-28T23:36:17.929Z",
-    "2020-08-01T10:51:36.790Z",
+    "2020-11-01T13:15:33.035Z",
+    "2020-11-30T09:48:16.867Z",
+    "2020-12-25T06:04:23.907Z",
+    "2022-01-25T14:18:46.235Z",
+    "2022-02-05T16:33:06.386Z",
+    "2022-11-10T14:43:26.374Z",
+    "2022-11-14T18:49:59.371Z",
+    "2022-11-15T15:01:20.894Z",
   ],
   currency: "EUR",
   locale: "pt-PT", // de-DE
@@ -63,14 +63,14 @@ const account2 = {
   pin: 2222,
 
   movementsDates: [
-    "2019-11-01T13:15:33.035Z",
-    "2019-11-30T09:48:16.867Z",
-    "2019-12-25T06:04:23.907Z",
-    "2020-01-25T14:18:46.235Z",
-    "2020-02-05T16:33:06.386Z",
-    "2020-04-10T14:43:26.374Z",
-    "2020-06-25T18:49:59.371Z",
-    "2020-07-26T12:01:20.894Z",
+    "2020-11-01T13:15:33.035Z",
+    "2020-11-30T09:48:16.867Z",
+    "2020-12-25T06:04:23.907Z",
+    "2022-01-25T14:18:46.235Z",
+    "2022-02-05T16:33:06.386Z",
+    "2022-06-10T14:43:26.374Z",
+    "2022-09-12T18:49:59.371Z",
+    "2022-11-15T15:01:20.894Z",
   ],
   currency: "USD",
   locale: "en-US",
@@ -114,13 +114,15 @@ let formatMovementDates = function (date) {
     console.log(DaysPassed);
 
  
-    // if (DaysPassed === 0) return 'Today';
-    // if (DaysPassed === 1) return 'Yesterday';
-    // if (DaysPassed <= 7) return `${DaysPassed}`
+    if (DaysPassed === 0) return 'Today';
+    if (DaysPassed === 1) return 'Yesterday';
+    if (DaysPassed <= 7) return `${DaysPassed} days ago.`
+    else {
     let day = `${date.getDate()}`.padStart(2, 0);
     let month = `${date.getMonth() + 1}`.padStart(2, 0);
     let year = date.getFullYear();
     return `${day}/${month}/${year}`
+    }
  
 }
 
